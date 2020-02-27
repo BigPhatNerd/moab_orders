5dfa1c84be75d5d80ae36438d2eb9f85048b4cf3
Wilson <wilsonhorrell@git.training>
2020-02-27 16:57:31 -0600

    changed production.rb file for sending email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.action_mailer.default_url_options = { host: 'moab-orders.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'mob-orders.herokuapp.com', port: 3000 }
 
 
   # Full error reports are disabled and caching is turned on.
